e666062186ced8bc33ba6dc6966b6fe8e94b8b1d
Priyanshu Jha <73241432+GitFlame@users.noreply.github.com>
2023-10-15 23:53:21 +0530

    Add files via upload

---
+++ Week_1/display.php
@@ -0,0 +1,136 @@
+<?php
+include 'connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/display_style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      min-height: 100vh;
+      background-color: #1BECE2;
+    }
+
+    .container {
+      flex-grow: 1;
+    }
+
+    footer {
+      text-align: center;
+    }
+  </style>
+</head>
+
+
+
+<body>
+  <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+      <img src="css/dashboard.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      Dashboard
+    </a>
+    <form action="logout.php" method="post" class="form-inline">
+      <button type="submit" class="btn btn-link text-light">Logout</button>
+    </form>
+  </nav>
+
+
+  <div class="container">
+    <button class="btn btn-primary my-5"><a href="index.php" class="text-light">+ Add User</a></button>
+
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Sl no</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email Id</th>
+          <th scope="col">Password</th>
+          <th scope="col">Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $sql = "Select * from `details`";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            $id = $row['id'];
+            $name = $row['username'];
+            $email = $row['email_id'];
+            $password = $row['password'];
+
+
+            // Check if the user is present in the database
+            $statusButton = (mysqli_num_rows($result) > 0) ? '<button class="btn btn-success">Active</button>' : '<button class="btn btn-danger">Inactive</button>';
+            echo '<tr>
+                        <th scope="row">' . $id . '</th>
+                        <td>' . $name . '</td>
+                        <td>' . $email . '</td>
+                        <td>' . $password . '</td>
+
+                        
+                        <td>
+
+                        <button class="btn btn-primary"><a href="update.php?update_id=' . $id . '" class="text-light">Update</a></button>
+
+                            <button class="btn btn-danger"><a href="delete.php?delete_id=' . $id . '" class="text-light">Delete</a></button>
+                            ' . $statusButton . '
+                  </td>
+                </tr>';
+          }
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+
+  <div class="modal fade" id="welcomeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Welcome to the Dashboard</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          This is your dashboard. Explore and manage your data here.
+        </div>
+
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    window.onload = function () {
+      $('#welcomeModal').modal('show');
+    }
+  </script>
+
+  <footer>
+    <p>&copy; 2023 Indusnet Technologies</p>
+    <p>All Copyright Reserved!</p>
+  </footer>
+
+  
+
+</body>
+
+</html>
