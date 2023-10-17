c86daaa958feeff51533901b7fa54b3cd0a29a6a
Priyanshu Jha <73241432+GitFlame@users.noreply.github.com>
2023-10-17 11:43:33 +0530

    delete.php

--- Week_1/delete.php
+++ Week_1/delete.php
@@ -2,27 +2,22 @@
 include 'connect.php';
 if(isset($_GET['delete_id'])){
     $id=$_GET['delete_id'];
-    echo "<script>
-            var confirmed = confirm('Are you sure you want to delete this record?');
-            if(confirmed) {
-                window.location.href='delete.php?confirmed=true&delete_id=$id';
-            } else {
-                window.location.href='display.php';
-            }
-          </script>";
-}
-
-if(isset($_GET['confirmed']) && $_GET['confirmed'] === 'true'){
-    $id=$_GET['delete_id'];
 
     $sql="Delete from `details` where id=$id";
     $result=mysqli_query($conn,$sql);
     if($result){
-        echo "<script>alert('Record Deleted');</script>";
-        echo "<script>setTimeout(function(){window.location.href='display.php'}, 2000);</script>";
+      echo "<script>
+            var confirmed = confirm('Are you sure you want to delete this record?');
+            if(confirmed) {
+                window.location.href='display.php?confirmed=true&delete_id=$id';
+            } else {
+                window.location.href='display.php';
+            }
+          </script>";
         exit();
-    } else {
+    }else{
         die(mysqli_error($con));
     }
+
 }
 ?>
