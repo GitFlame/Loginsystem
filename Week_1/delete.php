09bd803425c22693b2e40efa96bcc6baef42d1f8
Priyanshu Jha <73241432+GitFlame@users.noreply.github.com>
2023-10-17 12:09:44 +0530

    delete.php

--- Week_1/delete.php
+++ Week_1/delete.php
@@ -6,14 +6,8 @@ if(isset($_GET['delete_id'])){
     $sql="Delete from `details` where id=$id";
     $result=mysqli_query($conn,$sql);
     if($result){
-      echo "<script>
-            var confirmed = confirm('Are you sure you want to delete this record?');
-            if(confirmed) {
-                window.location.href='display.php?confirmed=true&delete_id=$id';
-            } else {
-                window.location.href='display.php';
-            }
-          </script>";
+        echo "<script>alert('Record Deleted');</script>";
+        echo "<script>setTimeout(function(){window.location.href='display.php'}, 500);</script>";
         exit();
     }else{
         die(mysqli_error($con));
