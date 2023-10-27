c503e0a8e7181f7ce1f1bd39afa01c9d67633a02
Priyanshu Jha <73241432+GitFlame@users.noreply.github.com>
2023-10-27 18:33:36 +0530

    toggle_active.php

--- Week_1/toggle_active.php
+++ Week_1/toggle_active.php
@@ -3,15 +3,23 @@ include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_POST['id'];
-
-    // Toggle the is_active status
-    $sql = "UPDATE details SET is_active = NOT is_active WHERE id = $id";
-    $result = mysqli_query($conn, $sql);
-
-    if ($result) {
-        echo json_encode(array('success' => true, 'is_active' => $result));
+    $status = $_POST['status'];
+    if ($status == 1) {
+        $sql_update_status = "UPDATE details SET is_active = '0' WHERE id = $id";
+        $result_update_status = mysqli_query($conn, $sql_update_status);
+        if ($result_update_status) {
+            echo json_encode(array('success' => true, 'is_active' => 0));
+        } else {
+            echo json_encode(array('success' => false, 'message' => 'Failed to update active status'));
+        }  
     } else {
-        echo json_encode(array('success' => false, 'message' => 'Failed to toggle active status'));
+        $sql_update_status = "UPDATE details SET is_active = '1' WHERE id = $id";
+        $result_update_status = mysqli_query($conn, $sql_update_status);
+        if ($result_update_status) {
+            echo json_encode(array('success' => true, 'is_active' => 1));
+        } else {
+            echo json_encode(array('success' => false, 'message' => 'Failed to update active status'));
+        }
     }
 } else {
     echo json_encode(array('success' => false, 'message' => 'Invalid request method'));
